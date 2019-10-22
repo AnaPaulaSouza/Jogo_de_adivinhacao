1bab2252c553ab9aecdd9e6ff1962a60b6b7c3bc
Souza <Ana.Souza@unisys.com>
2019-10-22 14:57:20 -0300

    correção de erro

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,37 +1,34 @@
 def boas_vindas
-    puts " \t\t Bem vindo ao jogo de adivinhação \t\t"
-    puts "\n"
+    puts " \t\tBem vindo ao jogo de adivinhação\n\n"
     puts "Qual o seu nome?"
     nome = gets
-    puts "\n\n"
-    puts "Começaremos o jogo, " + nome
+    puts "Prepare-se para começar o jogo, " + nome + "\n\n"
 end
 
-def sorteio_numero_secreto
-    puts "Escolhendo um número secreto entre 0 e 200..."
+def sorteia_numero_secreto
+    puts "Sortiando um número secreto entre 0 e 200..."
     sorteado = 175
-    puts "Número secreto escolhido... Que tal adivinhar nosso número secreto de hoje?"
+    puts "Número secreto sortiado... Que tal adivinha-lo?\n\n"
     sorteado
 end
 
 def digite_um_numero(tentativa, limite_de_tentativas)
-    puts "\n\n"
+    puts "\n"
     puts "\t Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
-    puts "Escolha um número"
+    puts "Escolha um número:"
     chute = gets
     puts "Será que você acertou? Você chutou " + chute
     chute
 end
 
-def verifica_se_acertou
-
-    acertou = sorteado == chute.to_i
+def verifica_se_acertou(numero_secreto, chute)
+    acertou = numero_secreto == chute.to_i
 
     if acertou
         puts "Parabêns. VOCÊ ACERTOU!"
         return true
     else 
-        maior = sorteado < chute.to_i
+        maior = numero_secreto < chute.to_i
         if maior
             puts "O número secreto maior"
             return false
@@ -43,11 +40,13 @@ def verifica_se_acertou
 end
 
 boas_vindas
-numero_secreto = sorteio_numero_secreto
+numero_secreto = sorteia_numero_secreto
 
 limite_de_tentativas = 5
 
 for tentativa in 1..limite_de_tentativas
     chute = digite_um_numero(tentativa, limite_de_tentativas)
-    verifica_se_acertou
+    if verifica_se_acertou(numero_secreto, chute)   
+        break
+    end
 end
